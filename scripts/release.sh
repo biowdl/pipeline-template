b7c65de85d2dd00b1c737ea4bad59080383339d4
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-11-29 10:04:25 +0100

    add extra stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,13 +10,18 @@ git checkout develop
 echo "Get latest develop branch"
 git pull origin develop
 echo "Check if all submodules are tagged"
+
 git submodule foreach --recursive \
 bash -c 'if [ "$(git tag --contains)" == "" ] ; \
-then echo "Untagged submodule found. Please make sure all submodules are released" && exit 1 \
-else echo "contains tag: $(git tag --contains)"'
-
+then echo "Untagged submodule found. Please make sure all submodules are released. Aborting release procedure." && exit 1 ;\
+else echo "contains tag: $(git tag --contains)" ;\
+fi'
 echo "Merge develop into master"
 git checkout master
 git pull origin master
 git merge origin/develop
-
+CURRENT_VERSION="$(cat VERSION)"
+echo "Version to be released = $CURRENT_VERSION"
+RELEASE_TAG="v$CURRENT_VERSION"
+echo "Tagging release: $RELEASE_TAG"
+git tag -a $RELEASE_TAG
