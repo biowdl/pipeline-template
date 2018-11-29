06886075e1d840c981fab6cd24efff5e3f9a022c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-11-29 10:19:50 +0100

    fully document release procedure in bash script

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@
 set -eu
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
+VERSION_FILE="VERSION"
 
 # CHECKING OUT LATEST VERSION OF DEVELOP
 cd $GIT_ROOT
@@ -18,8 +19,6 @@ bash -c 'if [ "$(git tag --contains)" == "" ] ; \
 then echo "Untagged submodule found. Please make sure all submodules are released. Aborting release procedure." && exit 1 ;\
 else echo "contains tag: $(git tag --contains)" ;\
 fi'
-# TODO: Add command that does a quick test of the pipeline.
-# Womtool validate maybe?
 
 # MERGING INTO MASTER
 echo "Merge develop into master"
@@ -27,8 +26,18 @@ git checkout master
 git pull origin master
 git merge origin/develop
 
+# Another check to see if after merging everything still is okay.
+echo "Check if all submodules are tagged"
+git submodule foreach --recursive \
+bash -c 'if [ "$(git tag --contains)" == "" ] ; \
+then echo "Untagged submodule found. Please make sure all submodules are released. Aborting release procedure." && exit 1 ;\
+else echo "contains tag: $(git tag --contains)" ;\
+fi'
+# TODO: Add command that does a quick test of the pipeline.
+# Womtool validate maybe?
+
 # SET RELEASE VERSION
-CURRENT_VERSION="$(cat VERSION)"
+CURRENT_VERSION="$(cat $VERSION_FILE)"
 echo "Version to be released = $CURRENT_VERSION"
 RELEASE_TAG="v$CURRENT_VERSION"
 echo "Tagging release: $RELEASE_TAG"
@@ -36,11 +45,29 @@ git tag -a $RELEASE_TAG
 
 echo "Tagging successfull"
 echo "push release to remote repository?"
-
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) git push origin master && git push origin $RELEASE_TAG; break;;
         No ) echo "release aborted" && exit 1;;
     esac
 done
+
+git checkout develop
+git merge master
+echo "Released version was: $CURRENT_VERSION"
+read -p "What should be the next version? " NEXT_VERSION
+echo "Setting next version to be: $NEXT_VERSION"
+echo "$NEXT_VERSION" > $VERSION_FILE
+git add $VERSION_FILE
+git commit -m "setting next version"
+
+echo "push develop with new version '$NEXT_VERSION' to remote repository?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) git push origin develop; break;;
+        No ) echo "remote push aborted" && exit 1;;
+    esac
+done
+echo "release procedure successful"
+
 
